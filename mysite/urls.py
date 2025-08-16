56c3a3e194782a7836a22d1664ba5081c8a38046
sadra nobari <sadra.nobari83@gmail.com>
2025-08-16 17:23:17 +0200

    creating templates folder - adding index and about page

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from firstapp.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", home),
+    path("home/", home),
+    path("about/", about)
+
+    
 ]
